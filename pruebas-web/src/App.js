3fc9b8b3fdb15d529dddf693932f348462643a62
AngeloIbaceta1 <72214315+AngeloIbaceta1@users.noreply.github.com>
2020-11-23 19:28:41 -0300

    a

--- pruebas-web/src/App.js
+++ pruebas-web/src/App.js
@@ -1,35 +1,11 @@
 import React from 'react';
 import './App.css';
 
-
 function App() {
   return (
     <div className="App">
-      <div className="botones">
-        <div className="btn1">
-          <label>
-            Opcion 1
-            <button className="opcion1" type="submit">
-              Computador gamer
-            </button>
-          </label>
-        </div>
-        <div className="btn2">
-          <label>
-            Opcion 2
-            <button className="opcion2" type="submit">
-              Computador de oficina
-            </button>
-          </label>
-        </div>
-        <div className="btn3">
-          <label>
-            Opcion 3
-            <button className="opcion3" type="submit">
-              Computador de renderizados
-            </button>
-          </label>
-        </div>
+      <div className="card1">
+        Hola
       </div>
     </div>
   );
