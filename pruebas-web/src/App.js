ff1738941488393381c0f8011f99bf4d50479672
AngeloIbaceta1 <72214315+AngeloIbaceta1@users.noreply.github.com>
2020-11-24 19:38:15 -0300

    nuevos js con los computadores

--- pruebas-web/src/App.js
+++ pruebas-web/src/App.js
@@ -1,42 +1,52 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Button } from "react-bootstrap"
+import { Card, Button, CardGroup } from "react-bootstrap";
+
 
 function App() {
   return (
     <div className="App">
-      <Card style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title>Computador Gamer</Card.Title>
-          <Card.Text>
-            Esta wea es un computador gamer
-          </Card.Text>
-          <Card.Img variant="top" src="https://cdn.discordapp.com/attachments/775904281671893042/779732562707742751/703-Negro-6.png" />
-          <Button variant="primary">ir al computador gamer</Button>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title>Computador Gamer</Card.Title>
-          <Card.Text>
-            Esta wea es un computador gamer
-          </Card.Text>
-          <Card.Img variant="top" src="https://cdn.discordapp.com/attachments/775904281671893042/779732562707742751/703-Negro-6.png" />
-          <Button variant="primary">ir al computador gamer</Button>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title>Computador Gamer</Card.Title>
-          <Card.Text>
-            Esta wea es un computador gamer
-          </Card.Text>
-          <Card.Img variant="top" src="https://cdn.discordapp.com/attachments/775904281671893042/779732562707742751/703-Negro-6.png" />
-          <Button variant="primary">ir al computador gamer</Button>
-        </Card.Body>
-      </Card>
-        
+      <div className="cards">
+        <CardGroup>
+          <Card>
+            <Card.Body>
+              <Card.Title>PC Gamer</Card.Title>
+              <Card.Text>
+              PC Gamer
+              </Card.Text>
+              <Card.Img variant="top" src="https://cdn.discordapp.com/attachments/775904281671893042/779732562707742751/703-Negro-6.png" />
+            </Card.Body>
+            <Card.Footer>
+              <Button variant="primary">ir al computador gamer</Button>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>PC de renderizados</Card.Title>
+              <Card.Text>
+                PC de renderizados
+              </Card.Text>
+              <Card.Img variant="top" src="https://cdn.discordapp.com/attachments/775904281671893042/779732562707742751/703-Negro-6.png" />
+            </Card.Body>
+            <Card.Footer>
+              <Button variant="primary">ir al computador de renderizados</Button>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>PC de oficina</Card.Title>
+              <Card.Text>
+                PC de oficina
+              </Card.Text>
+              <Card.Img variant="top" src="https://cdn.discordapp.com/attachments/775904281671893042/779732562707742751/703-Negro-6.png" />
+            </Card.Body>
+            <Card.Footer>
+              <Button variant="primary">ir al computador de oficina</Button>
+            </Card.Footer>
+          </Card>
+        </CardGroup>
+      </div>
     </div>
   );
 }
